cf81de9e256dc7e5fe2956c2fc276a1872f54475
chandrika <chandrika.kmc6@gmail.com>
2022-04-14 01:03:22 +0530

    add mod

--- addition.js
+++ addition.js
@@ -1,6 +1,6 @@
 function add()
 {
-var a=document.getElementbyId("firstnor").value
+var a=document.getElementbyId("firstno").value
 var b=document.getElementbyId("secondno").value
 var result
 result=a + b
