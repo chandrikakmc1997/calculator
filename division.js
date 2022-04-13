bdd7821aaf4104526ea4bfd47d61106fd608930b
chandrika <chandrika.kmc6@gmail.com>
2022-04-14 00:56:04 +0530

    div modified

--- division.js
+++ division.js
@@ -1,6 +1,6 @@
 function div() {
-var a =documet.getElementById("first no").value
-var b =document.getElementById("second no").value
+var a =documet.getElementById("firstno").value
+var b =document.getElementById("secondno").value
 var result 
 result =a/b
 document.getElementById("result").value=result
