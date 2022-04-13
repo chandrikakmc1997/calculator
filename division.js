43a889ead41b67b4206b9db556c2992428d953e2
sukhanya <sukhanyam3@gmail.com>
2022-04-13 17:49:21 +0530

    modified file

--- division.js
+++ division.js
@@ -1,6 +1,7 @@
 function div() {
-var a
-var b
+var a =documet.getElementById("first no").value
+var b =document.getElementById("second no").value
 var result 
 result =a/b
+document.getElementById("result").value=result
 }
