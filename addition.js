47a09669b509f0dbb4ab56883058d640d9d5f80b
chandini <ntchandini@gmail.com>
2022-04-14 11:10:39 +0530

    changed

--- addition.js
+++ addition.js
@@ -2,7 +2,7 @@ function add()
 {
 var a=(document.getElementById("firstno").value)
 var b=(document.getElementById("secondno").value)
-var result
+var result=parseInt(a) + parseInt(b)
 result=a + b
 document.getElementById("result").value=result
 }
