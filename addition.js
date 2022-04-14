b6257aa053aea6caab581b87a9af88921665963b
chandrika <chandrika.kmc6@gmail.com>
2022-04-14 09:39:02 +0530

    add edited

--- addition.js
+++ addition.js
@@ -4,7 +4,7 @@ var a=document.getElementbyId("firstno").value
 var b=document.getElementbyId("secondno").value
 var result
 result=a + b
-document.getElmentbyId("result").value=result
+document.getElementbyId("result").value=result
 
 }
 
