a51ce00bf4585adcd4b661a8bcadd1f9e665a5fa
chandini <ntchandini@gmail.com>
2022-04-14 10:11:40 +0530

    added

--- addition.js
+++ addition.js
@@ -1,6 +1,6 @@
 function add()
 {
-var a=document.getelementById("firstno").value
+var a=document.getlementById("firstno").value
 var b=document.getelementById("secondno").value
 var result
 result=a + b
