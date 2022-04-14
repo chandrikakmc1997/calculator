f0068e7e34b6875e7d33c39197955900e3e71009
sukhanya <sukhanyam3@gmail.com>
2022-04-14 12:32:36 +0530

    divi added

--- division.js
+++ division.js
@@ -1,7 +1,9 @@
-function division() {
-var a = documet.getElementById("firstno").value
-var b = document.getElementById("secondno").value
-var result 
+function division()
+{
+var a= document.getElementById("firstno").value
+var b= document.getElementById("secondno").value
+var result
 result = a / b
-document.getElementById("result").value=result
+document.getElementById("result").value= result
 }
+
