bd3cd1b2d197e93d747ca9d82b6c88891f7f094c
chandrika <chandrika.kmc6@gmail.com>
2022-04-14 09:23:39 +0530

    mul edited

--- mul.js
+++ mul.js
@@ -1,9 +1,9 @@
 function mul()
 {
 var a=document.getElemenById("firstno").value
-var b=document.getelEmentById("secondno").value
+var b=document.getElementById("secondno").value
 var result
 result=a * b
-document.getelementById("result").value=result
+document.getElementById("result").value=result
 }
 
